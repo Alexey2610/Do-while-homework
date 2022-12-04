d3e6f960f3d6e9a6588d0c01989485b7d2c629d0
Alexey2610 <aleksej26101998@gmail.com>
2022-12-04 21:02:49 +0300

    Do While
    Do while practice homework

---
+++ Do While homework/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 10;i<=20;i++)
+        System.out.println(i*i);
+  int j = 10;
+  while (j<=20) {
+      ;
+      System.out.println(j * j);
+      j++;
+  }
+
+    }
+}
